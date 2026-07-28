5236552abd3a71f60da11df9fc987c751de5dc62
Giovanna Maximo <giovannammximo@gmail.com>
2026-07-28 20:37:44 -0300

    Adiciona dataset devidamente filtrado

---
+++ src/explorar_dataset.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+dados = pd.read_csv("dados/spotify_trabalho.csv")
+
+print(dados.head())
+print()
+
+print("Quantidade de linhas e colunas:")
+print(dados.shape)
+print()
+
+print("Colunas:")
+print(dados.columns)
+print()
+
+print("Tipos dos dados:")
+print(dados.dtypes)
+
+print(dados["artist_name"].value_counts())
+
+print(dados.describe())
---
+++ src/filtrar_dataset.py
@@ -0,0 +1,36 @@
+import duckdb
+
+artistas = [
+    "Taylor Swift",
+    "Twenty One Pilots",
+    "Linkin Park",
+    "Michael Jackson",
+    "Bad Bunny",
+    "Sabrina Carpenter",
+    "Bring Me The Horizon",
+    "One Direction",
+    "The Neighbourhood",
+    "Tame Impala"
+]
+
+lista_artistas = "', '".join(artistas)
+
+dados = duckdb.sql(f"""
+    SELECT *
+    FROM 'dados/spotify-huge-audio-features.parquet'
+    WHERE artist_name IN ('{lista_artistas}')
+""").df()
+
+print("Antes da remoção de duplicatas:")
+print(dados.shape)
+
+# Remove músicas repetidas por causa de colaborações
+dados = dados.drop_duplicates(subset="track_id")
+
+print("Depois da remoção de duplicatas:")
+print(dados.shape)
+
+# Salva o dataset final do trabalho
+dados.to_csv("dados/spotify_trabalho.csv", index=False)
+
+print("Arquivo salvo com sucesso!")
