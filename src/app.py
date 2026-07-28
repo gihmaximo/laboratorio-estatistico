9b6d7eb2241d48332133305df50e148c23b9637f
Giovanna Maximo <giovannammximo@gmail.com>
2026-07-28 20:56:07 -0300

    Refina a estrutura do projeto e inicia a construção das funções da minhastats.py

---
+++ src/app.py
@@ -0,0 +1,4 @@
+from stats import minhastats 
+
+lista = [1, 2, 3, 4, 5]
+print("Média =",minhastats.media(lista))
---
+++ src/stats/minhastats.py
@@ -0,0 +1,44 @@
+def media(lista):
+    soma = 0
+
+    for numero in lista:
+        soma += numero
+
+    return soma / len(lista)
+
+
+def mediana(lista):
+    pass
+
+def moda(lista):
+    pass
+
+def amplitude(lista):
+    pass
+
+def variancia_populacional(lista):
+    pass
+
+def variancia_amostral(lista):
+    pass
+
+def desvio_padrao_populacional(lista):
+    pass
+
+def desvio_padrao_amostral(lista):
+    pass
+
+def quartis(lista):
+    pass
+
+def percentil(lista):
+    pass
+
+def coeficiente_variacao(lista):
+    pass
+
+def covariancia(lista):
+    pass
+
+def correlacao_pearson(lista):
+    pass
